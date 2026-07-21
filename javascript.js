233317196120acbc4791bee0b93e8c6e6e7c49ad
HIJAZ70 <muhammedhijashijas9233@gmail.com>
2026-07-21 12:33:10 +0530

    vibe,js implementation

---
+++ javascript.js
@@ -0,0 +1,37 @@
+console.log("hijas")
+// variable
+// keywords
+// var
+// let
+// const
+// let data = 10;
+// var type = "hijaz";
+// console.log(data)
+// Number
+let length = 16;
+let weight = 7.5;
+
+
+// Strings
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Boolean
+let x = true;
+let y = false;
+
+// Undefined
+let x;
+let y;
+
+// Null
+let x = null;
+let y = null;
+
+
+// Object
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array Object
+const cars = ["Saab", "Volvo", "BMW"];
+
